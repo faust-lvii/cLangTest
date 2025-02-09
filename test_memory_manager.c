a2701ef9e5f23709d8a79de482ce52af85448e46
faust-lvii <k.yusufokuducu@gmail.com>
2025-02-09 23:58:35 +0300

    problems C

--- test_memory_manager.c
+++ test_memory_manager.c
@@ -1,21 +1,24 @@
 #include "memory_manager.h"
 #include <stdio.h>
-#include <assert.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define NUM_THREADS 4
 #define NUM_ALLOCATIONS 1000
 #define MAX_ALLOCATION_SIZE 1024
 
 // Thread test fonksiyonu
-void* thread_test(void* arg) {
+DWORD WINAPI thread_test(LPVOID arg) {
     void* ptrs[NUM_ALLOCATIONS];
     
     // Rastgele boyutlarda bellek ayır
     for (int i = 0; i < NUM_ALLOCATIONS; i++) {
         size_t size = (rand() % MAX_ALLOCATION_SIZE) + 1;
         ptrs[i] = mm_alloc(size);
-        assert(ptrs[i] != NULL);
+        if (!ptrs[i]) {
+            printf("Bellek ayırma hatası!\n");
+            return 1;
+        }
         
         // Belleği test et - yazma ve okuma
         memset(ptrs[i], i % 256, size);
@@ -23,15 +26,23 @@ void* thread_test(void* arg) {
     
     // Bellekleri rastgele sırayla serbest bırak
     for (int i = NUM_ALLOCATIONS - 1; i >= 0; i--) {
-        assert(mm_free(ptrs[i]) == MM_SUCCESS);
+        if (mm_free(ptrs[i]) != MM_SUCCESS) {
+            printf("Bellek serbest bırakma hatası!\n");
+            return 1;
+        }
     }
     
-    return NULL;
+    return 0;
 }
 
 int main() {
+    srand((unsigned int)time(NULL));
+
     // Bellek yöneticisini başlat
-    assert(mm_init() == MM_SUCCESS);
+    if (mm_init() != MM_SUCCESS) {
+        printf("Bellek yöneticisi başlatılamadı!\n");
+        return 1;
+    }
     printf("Bellek yöneticisi başlatıldı.\n");
     
     // Basit test
@@ -40,34 +51,54 @@ int main() {
     void* ptr2 = mm_alloc(256);
     void* ptr3 = mm_alloc(512);
     
-    assert(ptr1 && ptr2 && ptr3);
+    if (!ptr1 || !ptr2 || !ptr3) {
+        printf("Bellek ayırma hatası!\n");
+        return 1;
+    }
     printf("3 bellek bloğu başarıyla ayrıldı.\n");
     
     mm_print_stats();
     
-    assert(mm_free(ptr2) == MM_SUCCESS);
+    if (mm_free(ptr2) != MM_SUCCESS) {
+        printf("Bellek serbest bırakma hatası!\n");
+        return 1;
+    }
     printf("Orta blok serbest bırakıldı.\n");
     
     void* ptr4 = mm_alloc(128);
-    assert(ptr4);
+    if (!ptr4) {
+        printf("Bellek ayırma hatası!\n");
+        return 1;
+    }
     printf("Yeni blok ayrıldı.\n");
     
     mm_print_stats();
     
-    assert(mm_free(ptr1) == MM_SUCCESS);
-    assert(mm_free(ptr3) == MM_SUCCESS);
-    assert(mm_free(ptr4) == MM_SUCCESS);
+    if (mm_free(ptr1) != MM_SUCCESS ||
+        mm_free(ptr3) != MM_SUCCESS ||
+        mm_free(ptr4) != MM_SUCCESS) {
+        printf("Bellek serbest bırakma hatası!\n");
+        return 1;
+    }
     
     // Multi-thread test
     printf("\n=== Multi-thread Test ===\n");
-    pthread_t threads[NUM_THREADS];
+    HANDLE threads[NUM_THREADS];
     
     for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_create(&threads[i], NULL, thread_test, NULL);
+        threads[i] = CreateThread(NULL, 0, thread_test, NULL, 0, NULL);
+        if (threads[i] == NULL) {
+            printf("Thread oluşturma hatası!\n");
+            return 1;
+        }
     }
     
+    // Tüm thread'lerin tamamlanmasını bekle
+    WaitForMultipleObjects(NUM_THREADS, threads, TRUE, INFINITE);
+    
+    // Thread handle'larını kapat
     for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+        CloseHandle(threads[i]);
     }
     
     printf("Multi-thread test tamamlandı.\n");
